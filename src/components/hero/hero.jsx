517d8955deb024280181f7c8d90e49a0a63e3d73
javad <javadht1990a@gmail.com>
2024-01-24 02:11:06 +0330

    Add the header text and style

--- src/components/hero/hero.jsx
+++ src/components/hero/hero.jsx
@@ -1,18 +1,22 @@
+import Header from './heroHeader';
 import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
-
 import Image from 'react-bootstrap/Image';
 
-function Hero(props) {
+function Hero() {
     return (
         <Container>
             <Row>
-                <Col>{props.children}</Col>
+                <Col><Header topDescription="Commited to top quality services"
+                    title="QUALITY REPAIR AND INSTALLATION OF YOUR HOME"
+                    shortDescription="Many desktop publishing packages and web page 
+                    editors now use Lorem Ipsum as their default model text, and a search for 
+                    'lorem ipsum' will uncover many web sites still in their infancy" /></Col>
                 <Col><Image src='../../src/assets/hero-repair.svg' className='py-5' /></Col>
-            </Row>
+            </Row >
 
-        </Container>
+        </Container >
     );
 }
 
---
+++ src/components/hero/heroHeader.jsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import styles from "./heroHeader.module.css"
+
+
+
+const Header = ({ topDescription, title, shortDescription }) => {
+
+    return (
+        <>
+
+            <div className='h-100 d-flex flex-column justify-content-center '>
+                <span style={{ color: "#F37649" }}>{topDescription}</span>
+                {/* Using ChatGPT */}
+                <h1 className={styles.mainTextStyle}>{title.split(' ').map((word, index) => (
+                    <span key={index} style={{ color: ['REPAIR', 'AND', 'INSTALLATION'].includes(word) ? '#F37649' : 'inherit' }}>
+                        {word}
+                        {index < title.split(' ').length - 1 && ' '}
+                    </span>
+                ))}</h1>
+                {/* Finished Using ChatGPT */}
+                <small style={{ color: "#888" }}>{shortDescription}</small>
+            </div>
+
+
+        </>
+    )
+}
+
+
+export default Header
