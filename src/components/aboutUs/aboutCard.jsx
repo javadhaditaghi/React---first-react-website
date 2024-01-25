00ffca0698588fc02f5b3d5d6061b7148cc6b0c9
javad <javadht1990a@gmail.com>
2024-01-25 11:57:13 +0330

    finish the about us section

--- src/components/aboutUs/aboutCard.jsx
+++ src/components/aboutUs/aboutCard.jsx
@@ -23,7 +23,10 @@ const AboutUsCard = ({ topDescription, title, shortDescription, bulletOne, bulle
                         <path fill-rule="evenodd" clip-rule="evenodd" d="M18.0893 5.00964C18.4147 5.33508 18.4147 5.86272 18.0893 6.18815L8.08928 16.1882C7.76384 16.5136 7.2362 16.5136 6.91077 16.1882L1.91076 11.1882C1.58533 10.8627 1.58533 10.3351 1.91076 10.0096C2.2362 9.6842 2.76384 9.6842 3.08928 10.0096L7.50002 14.4204L16.9108 5.00964C17.2362 4.6842 17.7638 4.6842 18.0893 5.00964Z" fill="#F37649" />
                     </svg>&nbsp;&nbsp;{bulletThree}</li>
                 </ul>
-                <StyledButton></StyledButton>
+                <div className="ps-4 pt-0">
+                    <StyledButton></StyledButton>
+                </div>
+
             </div>
         </>
     )
