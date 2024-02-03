b2218e40a232120de2eb5da896214da373d3624e
javad <javadht1990a@gmail.com>
2024-02-04 00:22:38 +0330

    Add the responsive design for the count up section

--- src/components/countUp/countUp.jsx
+++ src/components/countUp/countUp.jsx
@@ -8,11 +8,15 @@ import CountUpWrapper from "./countUpWrapper"
 const CountUp = (props) => {
     return (
         <CountUpWrapper>
-            <div className="numbers">
-                <CountUpCard number={105} title="PROJECTS" />
-                <CountUpCard number={517} title="LOCATIONS" />
-                <CountUpCard number={468} title="GROWTH" />
-                <CountUpCard number={468} title="HAPPY CUSTOMER" />
+            <div className="numbers d-flex flex-column flex-md-row justify-content-center">
+                <div className="d-flex flex-row">
+                    <CountUpCard number={105} title="PROJECTS" />
+                    <CountUpCard number={517} title="LOCATIONS" />
+                </div>
+                <div className="d-flex flex-row">
+                    <CountUpCard number={468} title="GROWTH" />
+                    <CountUpCard number={468} title="HAPPY CUSTOMER" />
+                </div>
             </div>
             <div className="d-flex justify-content-center">
                 <div className="countText">
