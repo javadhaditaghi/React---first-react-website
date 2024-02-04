1237836e3924d2833ce6c826cbb7ebec1be431f8
javad <javadht1990a@gmail.com>
2024-02-04 22:02:42 +0330

    Add the responsive form for call to action section

--- src/components/callToAction/callToActionCard.jsx
+++ src/components/callToAction/callToActionCard.jsx
@@ -10,14 +10,14 @@ const CallToActionCard = () => {
         <Container className='py-5 my-4'>
             <Card className='p-5'>
                 <Card.Body>
-                    <div className='d-flex justify-content-between align-items-center'>
-                        <div className='w-50'>
+                    <div className='d-flex flex-column flex-md-row justify-content-between align-items-center'>
+                        <div className='flexInfo-wrapper mb-3 mb-md-0'>
                             <Card.Title><h2 style={{ fontWeight: "800" }}>Ready To Launch Your Next Website?</h2></Card.Title>
                             <Card.Text>
                                 On the other hand, we denounce with righteous indignation and dislike men who are so beguiled and demoralized by the charms of pleasure of the moment
                             </Card.Text>
                         </div>
-                        <div className="text-end">
+                        <div className="text-center text-md-end">
                             <StyledButton ></StyledButton>
                             <p>Or, take a peek inside <a href=''>our service</a></p>
 
